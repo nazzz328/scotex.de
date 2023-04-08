c3f1f5893747c138993ac6f2e2c843b65cff0a4f
nazzz328 <nazzz328@gmail.com>
2023-04-08 20:21:46 +0500

    added basic slider functionality and smooth transitions, fixed reviews height for smaller screens

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 ///////   Read more (Lese weiter) functionality on reviews
 
 const textEls = document.querySelectorAll(".reviews__text");
+const mediaQuery = window.matchMedia("(max-width: 1100px)");
 textEls.forEach((textEl) => {
   const readMoreLink = document.createElement("a");
   readMoreLink.href = "#";
@@ -25,7 +26,8 @@ text-decoration-thickness: 1px;
   });
   readLessLink.addEventListener("click", function (e) {
     e.preventDefault();
-    textEl.style.maxHeight = `11.5rem`;
+    if (mediaQuery.matches) textEl.style.maxHeight = `8rem`;
+    else textEl.style.maxHeight = `11.5rem`;
     textEl.classList.remove("reviews__text-trunk");
     textEl.parentElement.removeChild(readLessLink);
     textEl.parentElement.appendChild(readMoreLink);
@@ -34,3 +36,32 @@ text-decoration-thickness: 1px;
     textEl.parentElement.appendChild(readMoreLink);
   }
 });
+
+/////// Slider functionality
+
+const sliderBtns = document.querySelectorAll(".article__gallery-button");
+const mainImg = document.querySelector(".article__gallery-main-image");
+let activeBtn;
+
+const changeActiveBtn = (btn) => {
+  if (activeBtn) activeBtn.classList.remove("article__gallery-button--active");
+  activeBtn = btn;
+  activeBtn.classList.add("article__gallery-button--active");
+};
+sliderBtns.forEach((btn) => {
+  if (btn.querySelector("img").src === mainImg.src) changeActiveBtn(btn);
+  btn.addEventListener("mouseenter", function (e) {
+    changeActiveBtn(btn);
+    const img = e.currentTarget
+      .closest(".article__gallery-button")
+      .querySelector("img");
+    mainImg.classList.add("article__gallery-main-image-fade-in");
+    setTimeout(() => {
+      mainImg.src = img.src;
+      mainImg.classList.remove("article__gallery-main-image-fade-in");
+      mainImg.classList.add("article__gallery-main-image-fade-in--active");
+    }, 200);
+    mainImg.classList.remove("article__gallery-main-image-fade-in--active");
+  });
+});
+console.log(activeBtn);
