5c089b1f763034eae6197b840e75b5890e40f7a3
nazzz328 <nazzz328@gmail.com>
2023-04-08 18:13:52 +0500

    added functionality to expand article reviews and smooth reveal, made some changes for smaller screens

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
+///////   Read more (Lese weiter) functionality on reviews
+
 const textEls = document.querySelectorAll(".reviews__text");
 textEls.forEach((textEl) => {
+  const readMoreLink = document.createElement("a");
+  readMoreLink.href = "#";
+  readMoreLink.textContent = "Lese weiter";
+  readMoreLink.style.cssText = `
+color: #719d0b;
+text-decoration-thickness: 1px;
+`;
+  const readLessLink = document.createElement("a");
+  readLessLink.href = "#";
+  readLessLink.textContent = "Weniger lesen";
+  readLessLink.style.cssText = `
+color: #719d0b;
+text-decoration-thickness: 1px;
+`;
+  readMoreLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    textEl.style.maxHeight = `60rem`;
+    textEl.classList.add("reviews__text-trunk");
+    textEl.parentElement.removeChild(readMoreLink);
+    textEl.parentElement.appendChild(readLessLink);
+  });
+  readLessLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    textEl.style.maxHeight = `11.5rem`;
+    textEl.classList.remove("reviews__text-trunk");
+    textEl.parentElement.removeChild(readLessLink);
+    textEl.parentElement.appendChild(readMoreLink);
+  });
   if (textEl.scrollHeight > textEl.offsetHeight) {
-    const readMoreLink = document.createElement("a");
-    readMoreLink.href = "#";
-    readMoreLink.textContent = "Lese weiter";
-    readMoreLink.style.cssText = `
-  color: #719d0b;
-  text-decoration-thickness: 1px;
-  `;
-    readMoreLink.addEventListener("click", function (event) {
-      event.preventDefault();
-      //   textEl.classList.add("reviews__text-trunk");
-      //   textEl.parentNode.removeChild(readMoreLink);
-    });
-
-    textEl.parentNode.appendChild(readMoreLink);
+    textEl.parentElement.appendChild(readMoreLink);
   }
 });
